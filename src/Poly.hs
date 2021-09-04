991fc49ddebbbe848858a8ecd0415b1f203d4980
Daniil Iaitskov <daniil.iaitskov@soostone.com>
2021-09-04 19:46:19 -0400

    part1 implement CryptoHash effect

--- src/Poly.hs
+++ src/Poly.hs
@@ -1,9 +1,26 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE GADTs #-}
+{-
+
+Todo:
+ - implement CryptoHash
+ - choose KVStore implementation (sqlite?)
+ - test
+ - app and opt-parse applicative
+ - logging with polysemy co-log
+-}
 module Poly where
+
+import Crypto.KDF.BCrypt as BC
+import Crypto.Random
 
 import Data.ByteString
 
 import Polysemy
+import Polysemy.Internal
 import Polysemy.KVStore
+import Polysemy.State
 
 ------------------------------------------------------------------------------
 newtype Username = Username ByteString
@@ -21,12 +38,14 @@ makeHash x =
 
 validateHash :: Member CryptoHash r => Password -> PasswordHash -> Sem r Bool
 validateHash password hash =
-  send (ValidateHash password passwordHash :: CryptoHash (Sem r) Bool)
+  send (ValidateHash password hash :: CryptoHash (Sem r) Bool)
 
 -- KVStore effect is already defined and provided from polysemy-zoo
 -- data KVStore k v m a
 
 ------------------------------------------------------------------------------
+-- business logic
+
 addUser
   :: Members [CryptoHash, KVStore Username PasswordHash] r
   => Username
@@ -46,3 +65,20 @@ validatePassword username password = do
   case hashInStore of
     Just h -> validateHash password h
     Nothing -> pure False
+
+
+------------------------------------------------------------------------------
+-- effect implementions
+
+runCryptoHashAsState
+  :: (DRG gen, Member (State gen) r)
+  => Sem (CryptoHash : r) a
+  -> Sem r a
+runCryptoHashAsState = interpret $ \case
+  ValidateHash (Password password) (PasswordHash hash) ->
+    pure (BC.validatePassword password hash)
+  MakeHash (Password password) -> do
+    drg <- get
+    let (h, drg') = withDRG drg (BC.hashPassword 10 password)
+    put drg'
+    pure $ PasswordHash h
